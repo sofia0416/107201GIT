8a93b381894bfa97469fd85ffbaa5d4d40966b18
Unknown <sofia0416@gmail.com>
2018-11-01 10:52:28 +0800

    程式打掉重來
    依然無法顯示，開始檢查套件or硬體

--- finall_nl_.ino
+++ finall_nl_.ino
@@ -1,74 +1,93 @@
-#include <SoftwareSerial.h>
+#include <Arduino.h>
+#define LENG 31   
+unsigned char buf[LENG];
 
-SoftwareSerial Serial1(2, 3); // RX, TX
+int PM01Value=0;          //define PM1.0
+int PM2_5Value=0;         //define PM2.5
+int PM10Value=0;         //define PM10
 
-long pmcf10=0;
-long pmcf25=0;
-long pmcf100=0;
-long pmat10=0;
-long pmat25=0;
-long pmat100=0;
 
-char buf[50];
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.setTimeout(1500);    
+
 }
 
-void loop() {
-  int count = 0;
-  unsigned char c;
-  unsigned char high;
-  while (Serial1.available()) {
-  c = Serial1.read();
-  if((count==0 && c!=0x42) || (count==1 && c!=0x4d)){
-  Serial.println("check failed");
-  break;
+void loop()
+{
+  if(Serial.find(0x42)){
+    Serial.readBytes(buf,LENG);
+
+    if(buf[0] == 0x4d){
+      if(checkValue(buf,LENG)){
+        PM01Value=transmitPM01(buf); //count PM1.0 value of the air detector module
+        PM2_5Value=transmitPM2_5(buf);//count PM2.5 value of the air detector module
+        PM10Value=transmitPM10(buf); //count PM10 value of the air detector module 
+      }           
+    } 
   }
-if(count > 15){
-  Serial.println("complete");
-  break;
-}
-else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14) high = c;
-else if(count == 5){
-  pmcf10 = 256*high + c;
-  Serial.print("CF=1, PM1.0=");
-  Serial.print(pmcf10);
-  Serial.println(" ug/m3");
-}
-else if(count == 7) {
-  pmcf25 = 256*high + c;
-  Serial.print("CF=1, PM2.5=");
-  Serial.print(pmcf25);
-  Serial.println(" ug/m3");
-}
-  else if(count == 9){
-  pmcf100 = 256*high + c;
-  Serial.print("CF=1, PM10=");
-  Serial.print(pmcf100);
-  Serial.println(" ug/m3");
-}
-  else if(count == 11){
-  pmat10 = 256*high + c;
-  Serial.print("atmosphere, PM1.0=");
-  Serial.print(pmat10);
-  Serial.println(" ug/m3");
-}
-  else if(count == 13){
-  pmat25 = 256*high + c;
-  Serial.print("atmosphere, PM2.5=");
-  Serial.print(pmat25);
-  Serial.println(" ug/m3");
-}
-  else if(count == 15){
-  pmat100 = 256*high + c;
-  Serial.print("atmosphere, PM10=");
-  Serial.print(pmat100);
-  Serial.println(" ug/m3");
-}
-count++;
 
-//7月10
-//開始G3程式，7688會跑出亂碼或0。可能是套件或硬體有問題?????????
+  static unsigned long OledTimer=millis();  
+    if (millis() - OledTimer >=1000) 
+    {
+      OledTimer=millis(); 
+      
+      Serial.print("PM1.0: ");  
+      Serial.print(PM01Value);
+      Serial.println("  ug/m3");            
+    
+      Serial.print("PM2.5: ");  
+      Serial.print(PM2_5Value);
+      Serial.println("  ug/m3");     
+      
+      Serial.print("PM1 0: ");  
+      Serial.print(PM10Value);
+      Serial.println("  ug/m3");   
+      Serial.println();
+    }
+  
+}
+char checkValue(unsigned char *thebuf, char leng)
+{  
+  char receiveflag=0;
+  int receiveSum=0;
 
+  for(int i=0; i<(leng-2); i++){
+  receiveSum=receiveSum+thebuf[i];
+  }
+  receiveSum=receiveSum + 0x42;
+ 
+  if(receiveSum == ((thebuf[leng-2]<<8)+thebuf[leng-1]))  
+  {
+    receiveSum = 0;
+    receiveflag = 1;
+  }
+  return receiveflag;
+}
+
+int transmitPM01(unsigned char *thebuf)
+{
+  int PM01Val;
+  PM01Val=((thebuf[3]<<8) + thebuf[4]); 
+  return PM01Val;
+}
+
+//transmit PM Value to PC
+int transmitPM2_5(unsigned char *thebuf)
+{
+  int PM2_5Val;
+  PM2_5Val=((thebuf[5]<<8) + thebuf[6]);
+  return PM2_5Val;
+  }
+
+//transmit PM Value to PC
+int transmitPM10(unsigned char *thebuf)
+{
+  int PM10Val;
+  PM10Val=((thebuf[7]<<8) + thebuf[8]); 
+  return PM10Val;
+}
+
+//0716
+//程式打掉重來，依然無法顯示，開始檢查套件or硬體
