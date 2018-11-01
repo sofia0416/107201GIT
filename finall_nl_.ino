d6cbe4fa00aabac7356691f80136cb96a046cc85
Unknown <sofia0416@gmail.com>
2018-11-01 10:53:29 +0800

    在git找到7688 + G3的.h
    編寫設定讓它顯示

--- finall_nl_.ino
+++ finall_nl_.ino
@@ -1,93 +1,46 @@
-#include <Arduino.h>
-#define LENG 31   
-unsigned char buf[LENG];
+#include <PMS.h>
+#include <SoftwareSerial.h>
 
-int PM01Value=0;          //define PM1.0
-int PM2_5Value=0;         //define PM2.5
-int PM10Value=0;         //define PM10
+SoftwareSerial pmsSerial(2, 3);
+PMS pms(pmsSerial);
+PMS::DATA pmsData;
 
+
+int pms_read_PM1()
+{
+  pms.read(pmsData);
+  return pmsData.PM_AE_UG_1_0;
+}
+
+int pms_read_PM2()
+{
+  pms.read(pmsData);
+  return pmsData.PM_AE_UG_2_5;
+}
+
+int pms_read_PM10()
+{
+  pms.read(pmsData);
+  return pmsData.PM_AE_UG_10_0;
+}
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.setTimeout(1500);    
-
+  pmsSerial.begin(9600);
 }
 
 void loop()
 {
-  if(Serial.find(0x42)){
-    Serial.readBytes(buf,LENG);
-
-    if(buf[0] == 0x4d){
-      if(checkValue(buf,LENG)){
-        PM01Value=transmitPM01(buf); //count PM1.0 value of the air detector module
-        PM2_5Value=transmitPM2_5(buf);//count PM2.5 value of the air detector module
-        PM10Value=transmitPM10(buf); //count PM10 value of the air detector module 
-      }           
-    } 
-  }
-
-  static unsigned long OledTimer=millis();  
-    if (millis() - OledTimer >=1000) 
-    {
-      OledTimer=millis(); 
-      
-      Serial.print("PM1.0: ");  
-      Serial.print(PM01Value);
-      Serial.println("  ug/m3");            
-    
-      Serial.print("PM2.5: ");  
-      Serial.print(PM2_5Value);
-      Serial.println("  ug/m3");     
-      
-      Serial.print("PM1 0: ");  
-      Serial.print(PM10Value);
-      Serial.println("  ug/m3");   
-      Serial.println();
-    }
-  
-}
-char checkValue(unsigned char *thebuf, char leng)
-{  
-  char receiveflag=0;
-  int receiveSum=0;
-
-  for(int i=0; i<(leng-2); i++){
-  receiveSum=receiveSum+thebuf[i];
-  }
-  receiveSum=receiveSum + 0x42;
- 
-  if(receiveSum == ((thebuf[leng-2]<<8)+thebuf[leng-1]))  
-  {
-    receiveSum = 0;
-    receiveflag = 1;
-  }
-  return receiveflag;
+  Serial.print("PM1.0:");
+  Serial.println(pms_read_PM1());
+  Serial.print("PM2.5:");
+  Serial.println(pms_read_PM2());
+  Serial.print("PM10:");
+  Serial.println(pms_read_PM1());
+  delay(1000);
 }
 
-int transmitPM01(unsigned char *thebuf)
-{
-  int PM01Val;
-  PM01Val=((thebuf[3]<<8) + thebuf[4]); 
-  return PM01Val;
-}
 
-//transmit PM Value to PC
-int transmitPM2_5(unsigned char *thebuf)
-{
-  int PM2_5Val;
-  PM2_5Val=((thebuf[5]<<8) + thebuf[6]);
-  return PM2_5Val;
-  }
+//在git找到7688+g3的.h，在編寫設定讓它顯示
 
-//transmit PM Value to PC
-int transmitPM10(unsigned char *thebuf)
-{
-  int PM10Val;
-  PM10Val=((thebuf[7]<<8) + thebuf[8]); 
-  return PM10Val;
-}
-
-//0716
-//程式打掉重來，依然無法顯示，開始檢查套件or硬體
