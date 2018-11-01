6d508559e043ded1c8e7d70ee7cb6e4cdbde833d
Unknown <sofia0416@gmail.com>
2018-11-01 10:51:27 +0800

    開始G3程式
    7688會跑出亂碼或0
    程式需要修改

--- finall_nl_.ino
+++ finall_nl_.ino
@@ -1,69 +1,74 @@
-#define BLYNK_PRINT Serial
-#include <Bridge.h>
-#include <BlynkSimpleYun.h>
-#include <DHT.h>
+#include <SoftwareSerial.h>
 
-char auth[] = "8320b6af67fd49f4b6f5a6b727f5d573";
+SoftwareSerial Serial1(2, 3); // RX, TX
 
-#define DHTPIN A1          // What digital pin we're connected to
+long pmcf10=0;
+long pmcf25=0;
+long pmcf100=0;
+long pmat10=0;
+long pmat25=0;
+long pmat100=0;
 
+char buf[50];
 
-// Uncomment whatever type you're using!
-
-#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
-
-DHT dht(DHTPIN, DHTTYPE);
-BlynkTimer timer;
-
-
-void sendSensor()
-{
-  float h = dht.readHumidity();
-  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
-  
-  Serial.print("DHT-Humidity: "); 
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("DHT-Temperature: "); 
-  Serial.print(t);
-  Serial.println(" *C\t");
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-   if (t > 38){
-    digitalWrite(5, HIGH);
-  }
-   else if (h < 40){
-    digitalWrite(5, HIGH);
-   }
-   else{
-    digitalWrite(5, LOW);
-  }
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V5, h);
-  Blynk.virtualWrite(V6, t);
-  Bridge.put("Humidity", String(h));
-  Bridge.put("Temperature", String(t));
-  }
-
-
-void setup()
-{
-  // Debug console
+void setup() {
   Serial.begin(9600);
-  Blynk.begin(auth);
-  dht.begin();
-  timer.setInterval(1000L, sendSensor);
-  Bridge.begin();
+  Serial1.begin(9600);
 }
 
-void loop()
-{
-  delay(1000);
-  Blynk.run();
-  timer.run();
+void loop() {
+  int count = 0;
+  unsigned char c;
+  unsigned char high;
+  while (Serial1.available()) {
+  c = Serial1.read();
+  if((count==0 && c!=0x42) || (count==1 && c!=0x4d)){
+  Serial.println("check failed");
+  break;
+  }
+if(count > 15){
+  Serial.println("complete");
+  break;
 }
-//六月初
-//測試DHT22 AND 使用ARDUINO
+else if(count == 4 || count == 6 || count == 8 || count == 10 || count == 12 || count == 14) high = c;
+else if(count == 5){
+  pmcf10 = 256*high + c;
+  Serial.print("CF=1, PM1.0=");
+  Serial.print(pmcf10);
+  Serial.println(" ug/m3");
+}
+else if(count == 7) {
+  pmcf25 = 256*high + c;
+  Serial.print("CF=1, PM2.5=");
+  Serial.print(pmcf25);
+  Serial.println(" ug/m3");
+}
+  else if(count == 9){
+  pmcf100 = 256*high + c;
+  Serial.print("CF=1, PM10=");
+  Serial.print(pmcf100);
+  Serial.println(" ug/m3");
+}
+  else if(count == 11){
+  pmat10 = 256*high + c;
+  Serial.print("atmosphere, PM1.0=");
+  Serial.print(pmat10);
+  Serial.println(" ug/m3");
+}
+  else if(count == 13){
+  pmat25 = 256*high + c;
+  Serial.print("atmosphere, PM2.5=");
+  Serial.print(pmat25);
+  Serial.println(" ug/m3");
+}
+  else if(count == 15){
+  pmat100 = 256*high + c;
+  Serial.print("atmosphere, PM10=");
+  Serial.print(pmat100);
+  Serial.println(" ug/m3");
+}
+count++;
+
+//7月10
+//開始G3程式，7688會跑出亂碼或0。可能是套件或硬體有問題?????????
+
